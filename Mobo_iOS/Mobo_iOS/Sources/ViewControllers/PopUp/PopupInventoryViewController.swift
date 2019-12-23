82e575919b471d228eea0cb435bc01f3af610731
Chokyungjin <46750574+chokyungjin@users.noreply.github.com>
2019-12-23 19:24:44 +0900

    Update

---
+++ Mobo_iOS/Mobo_iOS/Sources/ViewControllers/PopUp/PopupInventoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PopupInventory.swift
+//  Mobo_iOS
+//
+//  Created by 조경진 on 2019/12/23.
+//  Copyright © 2019 조경진. All rights reserved.
+//
+
+import UIKit
+
+class PopupInventoryViewController: UIViewController {
+    
+    @IBOutlet weak var MoveBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        MoveBtn.backgroundColor = .mainOragne
+        
+    }
+}
--- Mobo_iOS/Mobo_iOS/Sources/ViewControllers/ViewController.swift
+++ Mobo_iOS/Mobo_iOS/Sources/ViewControllers/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import Lottie
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var AnimationView: UIView!
+    @IBOutlet weak var PopupBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,18 +20,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
-
+    
     func startAnimation() {
         let animationView = Lottie.AnimationView(name:"loader")
-              
-              animationView.frame = CGRect(x:0, y:0, width:400, height:400)
-              animationView.center = self.view.center
-              animationView.contentMode = .scaleAspectFill
-              
-              AnimationView.addSubview(animationView)
-              animationView.play()
+        
+        animationView.frame = CGRect(x:0, y:0, width:400, height:400)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFill
+        
+        AnimationView.addSubview(animationView)
+        animationView.play()
     }
     
-
+    @IBAction func PopUpAct(_ sender: Any) {
+        
+        let storyboard = UIStoryboard(name: "PopUpScreen", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "PopupVC") as! PopupInventoryViewController
+        vc.modalPresentationStyle = .fullScreen //or .overFullScreen for transparency
+        
+        self.present(vc, animated: true, completion: nil)
+        
+    }
+    
+    
+    
 }
 
