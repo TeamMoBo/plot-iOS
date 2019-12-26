a1b027ee99522d1c6aba30fcde24258d203e1e6e
CheonYuJeong <ujcheon@sookmyung.ac.kr>
2019-12-27 07:06:45 +0900

    졸려

---
+++ Mobo_iOS/Mobo_iOS/Sources/View/MyPage/MyPageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MyPageViewController.swift
+//  Mobo_iOS
+//
+//  Created by 천유정 on 27/12/2019.
+//  Copyright © 2019 조경진. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+   
+}
+
+
+
