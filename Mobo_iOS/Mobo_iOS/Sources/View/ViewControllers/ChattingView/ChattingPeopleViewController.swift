cccd32dc2dd891796d7871367fcd159793aaaffc
Chokyungjin <46750574+chokyungjin@users.noreply.github.com>
2019-12-26 14:50:48 +0900

    chatting imple2

--- Mobo_iOS/Mobo_iOS/Sources/View/ViewControllers/ChattingView/ChattingPeopleViewController.swift
+++ Mobo_iOS/Mobo_iOS/Sources/View/ViewControllers/ChattingView/ChattingPeopleViewController.swift
@@ -11,120 +11,134 @@ import Firebase
 class ChattingPeopleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var array : [UserModel] = []
-        var tableview : UITableView!
-        override func viewDidLoad() {
-            super.viewDidLoad()
+    var tableview : UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableview = UITableView()
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.register(PeopleViewTableCell.self, forCellReuseIdentifier: "Cell")
+        view.addSubview(tableview)
+        tableview.snp.makeConstraints { (m) in
+            m.top.equalTo(view)
+            m.bottom.left.right.equalTo(view)
             
-            tableview = UITableView()
-            tableview.delegate = self
-            tableview.dataSource = self
-            tableview.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-            view.addSubview(tableview)
-            tableview.snp.makeConstraints { (m) in
-                m.top.equalTo(view)
-                m.bottom.left.right.equalTo(view)
+        }
+        
+        
+        Database.database().reference().child("users").observe(DataEventType.value, with: { (snapshot) in
+            
+            
+            self.array.removeAll()
+            let myUid = Auth.auth().currentUser?.uid
+            
+            for child in snapshot.children{
+                let fchild = child as! DataSnapshot
+                let userModel = UserModel()
+                
+                userModel.setValuesForKeys(fchild.value as! [String : Any])
+                
+                if(userModel.uid == myUid){
+                    continue
+                }
+                
+                self.array.append(userModel)
                 
             }
             
-            
-            Database.database().reference().child("users").observe(DataEventType.value, with: { (snapshot) in
-                
-                
-                self.array.removeAll()
-                let myUid = Auth.auth().currentUser?.uid
-
-                for child in snapshot.children{
-                    let fchild = child as! DataSnapshot
-                    let userModel = UserModel()
-                    
-                    userModel.setValuesForKeys(fchild.value as! [String : Any])
-                    
-                    if(userModel.uid == myUid){
-                        continue
-                    }
-                    
-                    self.array.append(userModel)
-                    
-                }
-                
-                DispatchQueue.main.async {
-                    self.tableview.reloadData();
-                }
-            })
-            
-            
-            
-            
-
-            // Do any additional setup after loading the view.
+            DispatchQueue.main.async {
+                self.tableview.reloadData();
+            }
+        })
+        
+        
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableview.dequeueReusableCell(withIdentifier: "Cell", for :indexPath) as! PeopleViewTableCell
+        
+        
+        let imageview = cell.imageview!
+        
+        imageview.snp.makeConstraints { (m) in
+            m.centerY.equalTo(cell)
+            m.left.equalTo(cell).offset(10)
+            m.height.width.equalTo(50)
         }
         
         
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return array.count
+        URLSession.shared.dataTask(with: URL(string: array[indexPath.row].profileImageUrl!)!) { (data, response, err) in
+            
+            
+            DispatchQueue.main.async {
+                imageview.image = UIImage(data: data!)
+                imageview.layer.cornerRadius = imageview.frame.size.width/2
+                imageview.clipsToBounds = true
+            }
+            
+        }.resume()
+        
+        let label = cell.label!
+        label.snp.makeConstraints { (m) in
+            m.centerY.equalTo(cell)
+            m.left.equalTo(imageview.snp.right).offset(20)
         }
         
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
-            
-           let cell = tableview.dequeueReusableCell(withIdentifier: "Cell", for :indexPath)
-                  
-                  let imageview = UIImageView()
-            
-                  cell.addSubview(imageview)
-                  imageview.snp.makeConstraints{(m) in
-                      m.centerY.equalTo(cell)
-                      m.left.equalTo(cell)
-                      m.height.width.equalTo(50)
-                  }
-                  
-                          URLSession.shared.dataTask(with: URL(string: array[indexPath.row].profileImageUrl!)!) { (data, response, err) in
-                  
-                              DispatchQueue.main.async {
-                                  imageview.image = UIImage(data: data!)
-                                  imageview.layer.cornerRadius = imageview.frame.size.width/2
-                                  imageview.clipsToBounds = true
-                              }
-                          }.resume()
-                  
-                  let label = UILabel()
-                  cell.addSubview(label)
-                  label.snp.makeConstraints{ (m) in
-                      m.centerY.equalTo(cell)
-                      m.left.equalTo(imageview.snp.right).offset(30)
-                  }
-                  
-                  label.text = array[indexPath.row].userName
-                  
-                  return cell
-              }
-              func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-                  return 70
-              }
-              
-              func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                  
-                 let view = self.storyboard?.instantiateViewController(withIdentifier: "ChatViewController") as? ChatViewController
-                
-                  view?.destinationUid = self.array[indexPath.row].uid
-                
-                  self.navigationController?.pushViewController(view!, animated: true)
-              }
-              
-              override func didReceiveMemoryWarning() {
-                  super.didReceiveMemoryWarning()
-                  // Dispose of any resources that can be recreated.
-              }
-              
-              
-              /*
-               // MARK: - Navigation
-               
-               // In a storyboard-based application, you will often want to do a little preparation before navigation
-               override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-               // Get the new view controller using segue.destinationViewController.
-               // Pass the selected object to the new view controller.
-               }
-               */
-              
-          }
+        label.text = array[indexPath.row].userName
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let view = self.storyboard?.instantiateViewController(withIdentifier: "ChatViewController") as? ChatViewController
+        
+        view?.destinationUid = self.array[indexPath.row].uid
+        
+        self.navigationController?.pushViewController(view!, animated: true)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+
+
+class PeopleViewTableCell :UITableViewCell{
+    
+    var imageview :UIImageView! = UIImageView()
+    var label :UILabel! = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addSubview(imageview)
+        self.addSubview(label)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
