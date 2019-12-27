aaf05a2f8cb2312f46dbb76cf4b39e316b2ab74e
CheonYuJeong <ujcheon@sookmyung.ac.kr>
2019-12-28 04:13:46 +0900

    wf

--- Mobo_iOS/Mobo_iOS/Sources/View/MyPage/MyPageViewController.swift
+++ Mobo_iOS/Mobo_iOS/Sources/View/MyPage/MyPageViewController.swift
@@ -10,37 +10,95 @@ import UIKit
 
 class MyPageViewController: UIViewController {
     
+    let picker = UIImagePickerController()
     
+    @IBOutlet weak var camera: UIButton!
+    
+    
+    @IBOutlet weak var profilepic: UIImageView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationSetup()
+              super.viewDidLoad()
+              navigationSetup()
+        picker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+          }
+       
+    @IBAction func camerabtn(_ sender: Any) {
+        let alert =  UIAlertController(title: "너를보여줘!", message: "인생사진 어때?", preferredStyle: .actionSheet)
+        
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary()
+        }
+        
+        let camera =  UIAlertAction(title: "카메라", style: .default) { (action) in
+            self.openCamera()
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        
+        
+        
         
     }
-
+    
+    
+    
+    func openLibrary(){
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)){
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+        }
+        else{
+            print("Camera not available")
+        }
+        
+    }
+    
+    
+    
+    
     func navigationSetup() { //네비게이션 투명색만들기
-               
-               self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 255/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
-               self.navigationController?.navigationBar.backIndicatorImage = #imageLiteral(resourceName: "iconsDarkBack")
-               self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = #imageLiteral(resourceName: "iconsDarkBack")
-               self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "채팅대기", style: .done, target: nil, action: nil)
-               self.navigationItem.backBarButtonItem?.tintColor = .black
-               //투명하게 만드는 공식처럼 기억하기
-               self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-               //네비게이션바의 백그라운드색 지정. UIImage와 동일
-               self.navigationController?.navigationBar.shadowImage = UIImage()
-               //shadowImage는 UIImage와 동일. 구분선 없애줌.
-               self.navigationController?.navigationBar.isTranslucent = true
-               //false면 반투명이다.
-               self.navigationController?.view.backgroundColor = UIColor.white.withAlphaComponent(0.0)
-               //뷰의 배경색 지정
-       //        self.navigationController?.navigationBar.topItem?.title = "Home"
-       //        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.init(red: 211/255.0, green: 211.0/255.0, blue: 211.0/255.0, alpha: 1.0)]
-       //        navigationController?.navigationBar.titleTextAttributes = textAttributes
-               
-           }
-   
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 255/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.backIndicatorImage = #imageLiteral(resourceName: "iconsDarkBack")
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = #imageLiteral(resourceName: "iconsDarkBack")
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "채팅대기", style: .done, target: nil, action: nil)
+        self.navigationItem.backBarButtonItem?.tintColor = .black
+        //투명하게 만드는 공식처럼 기억하기
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        //네비게이션바의 백그라운드색 지정. UIImage와 동일
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        //shadowImage는 UIImage와 동일. 구분선 없애줌.
+        self.navigationController?.navigationBar.isTranslucent = true
+        //false면 반투명이다.
+        self.navigationController?.view.backgroundColor = UIColor.white.withAlphaComponent(0.0)
+        //뷰의 배경색 지정
+        //        self.navigationController?.navigationBar.topItem?.title = "Home"
+        //        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.init(red: 211/255.0, green: 211.0/255.0, blue: 211.0/255.0, alpha: 1.0)]
+        //        navigationController?.navigationBar.titleTextAttributes = textAttributes
+        
+    }
+    
 }
-
-
-
+extension ViewController : UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+      
+      if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+      {
+          profilepic.image = image
+          print(info)
+          
+      }
+      dismiss(animated: true, completion: nil)
+    
+    
+}
