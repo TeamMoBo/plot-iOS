2b5cc1c47a468624a0df8315a140d5306d146b8f
Chokyungjin <46750574+chokyungjin@users.noreply.github.com>
2019-12-31 19:28:41 +0900

    code update

--- Mobo_iOS/Mobo_iOS/Sources/View/Sign/SignUpLastViewController.swift
+++ Mobo_iOS/Mobo_iOS/Sources/View/Sign/SignUpLastViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class SignUpLastViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -21,16 +21,22 @@ class SignUpLastViewController: UIViewController {
         //return 버튼 누르면 키보드 내려갈수있게 설정.
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextbtn(_ sender: Any) {
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = mainStoryboard.instantiateViewController(withIdentifier: "MainNaviVC") as! UINavigationController
+        vc.modalPresentationStyle = .fullScreen
+        self.show(vc, sender: nil)
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
