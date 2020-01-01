b9d1a629d8aa40bdbe9cd81164b57c8a949cf00d
CheonYuJeong <ujcheon@sookmyung.ac.kr>
2020-01-01 19:54:44 +0900

    채ㅡㅑㅜ

--- Mobo_iOS/Mobo_iOS/Resources/APIServices/Sign/ResponseString.swift
+++ Mobo_iOS/Mobo_iOS/Resources/APIServices/Sign/ResponseString.swift
@@ -10,12 +10,24 @@ import Foundation
 
 // MARK: - ResponseString // 성공했을 때 response body
 struct ResponseString: Codable {
-    let success: Bool
+    let status: Int
     let message: String
     let data: DataClass?
+    
+    enum CodinKeys: String, CodingKey {
+        case status
+        case message
+        case data
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        status = (try? values.decode(String.self, forKey: .status)) ?? ""
+        message = (try? values.decode(String.self, fotKey: .message)) ?? ""
+        data = (try? values.decode(String.self, forKey: .data))
+    }
 }
 // MARK: - DataClass
 struct DataClass: Codable {
-    let userIdx: Int
-    let id, password, name, phone: String
+    let token : String
 }
