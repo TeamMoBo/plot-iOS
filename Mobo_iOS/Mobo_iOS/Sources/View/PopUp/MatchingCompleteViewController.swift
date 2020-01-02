7957b577ab786ebbac354f25262e6a84ab61b019
CheonYuJeong <ujcheon@sookmyung.ac.kr>
2020-01-02 19:20:11 +0900

    popup 중간

---
+++ Mobo_iOS/Mobo_iOS/Sources/View/PopUp/MatchingCompleteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MatchingCompleteViewController.swift
+//  Mobo_iOS
+//
+//  Created by 천유정 on 02/01/2020.
+//  Copyright © 2020 조경진. All rights reserved.
+//
+
+import UIKit
+
+class MatchingCompleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
